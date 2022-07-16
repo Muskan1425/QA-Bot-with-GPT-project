14edc38a8ded5cd604c46dd17970488b6a8d766f
Muskan <muskanthakur1408@gmail.com>
2022-07-16 16:46:09 +0530

    Initial comment

--- evenodd.py
+++ evenodd.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 st.title('Python program to check if the input number is odd or even.')
 df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
+
 st.image('https://thumbs.dreamstime.com/z/titanic-iceberg-original-oil-painting-ocean-night-canvas-full-moon-stars-modern-impressionism-200487951.jpg')
   
 def func(num):
